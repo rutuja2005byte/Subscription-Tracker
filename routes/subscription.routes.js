a3ca23203490c36d00dfde02fc7bc376def0bd6b
Rutuja <rutujadarade2005@gmail.com>
2026-06-02 18:01:54 +0530

    Add GET route for subscription details (path typo)
    Add a new GET route that responds with subscription details. The route is registered as '/id:' and returns {title: 'GET subscription details'}. Note: the path appears malformed (likely intended to be '/:id'), so verify and correct the route parameter syntax as needed.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -6,4 +6,8 @@ subscription.get ('/', (req, res) =>
     res.send({title: 'GET all subcription'})
 );
 
+subscription.get('/id:', (req, res) =>
+    res.send({title: 'GET subscription details'})
+);
+
 export default subscriptionRouter;
