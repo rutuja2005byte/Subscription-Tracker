423c07442df22befddb3939db73c93ec063b7726
Rutuja <rutujadarade2005@gmail.com>
2026-06-04 09:04:34 +0530

    Add auth middleware and update user routes
    Introduce middlewares/auth.middleware.js to verify Bearer JWTs (using JWT_SECRET), fetch the user by decoded userId, attach req.user and return 401 on failure. Update routes/user.routes.js to import getUser/getUsers from the controller and replace the inline GET '/' handler with getUsers.

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
+import { getUser, getUsers } from '../controllers/user.controller.js';
+
 const userRouter = Router();
 
-userRouter.get ('/',(req ,res) => 
-    res.send({title: 'GET all users'})    
-);
+userRouter.get ('/', getUsers);
+
 
 userRouter.get ('/:id',(req , res) => 
     res.send({title: 'GET user details'})
