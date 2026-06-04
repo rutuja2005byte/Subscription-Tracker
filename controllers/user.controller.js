4654edbca454a8d7bf07dfd6b76bc416ea03cc85
Rutuja <rutujadarade2005@gmail.com>
2026-06-04 08:42:10 +0530

    Fetch user by ID and add 404 handling
    Update getUser to use User.findById(req.params.id) and exclude the password field via .select('-password'). Add a not-found check that throws a 404 error when the user does not exist. This replaces the previous use of User.find() which returned all users.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -12,7 +12,13 @@ export const getUsers = async (req, res, next) => {
 
 export const getUser = async (req, res, next) => {
     try {
-        const users = await User.find();
+        const user = await User.findById(req.params.id).select('-password');
+
+        if(!user){
+            const error = new Error('User not found');
+            error.statusCode = 404;
+            throw error;
+        }
 
         res.send(200).json({ success: true, data: users});
     } catch (error) {
