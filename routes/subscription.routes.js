e09a17cc008ee99c4c8da30ff3a84a1964759cff
Rutuja <rutujadarade2005@gmail.com>
2026-06-04 17:27:12 +0530

    Authorize GET /user/:id and use controller
    Import getUserSubscriptions from the subscription controller and replace the stubbed /user/:id route with an authorized route that calls getUserSubscriptions. This removes the placeholder response and wires the real controller handler into routes/subscription.routes.js.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import authorize from '../middlewares/auth.middleware.js';
-import { createSubscription } from '../controllers/subscription.controller.js';
+import { createSubscription, getUserSubscriptions } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
 
@@ -22,9 +22,7 @@ subscriptionRouter.delete('/:id', (req, res) =>
     res.send({title: 'DELETE subscription'})
 );
 
-subscriptionRouter.get('/user/:id', (req ,res) =>
-    res.send({title: 'GET all user subscriptions'})
-);
+subscriptionRouter.get('/user/:id', authorize, getUserSubscriptions);
 
 subscriptionRouter.put('/:id/cancel', (req ,res) =>
     res.send({title: 'CANCEL subscription'})
