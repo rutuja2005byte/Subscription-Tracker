10272d9b9212088892dda561962af916e3cccd92
Rutuja <rutujadarade2005@gmail.com>
2026-06-04 17:13:25 +0530

    Add getUserSubscriptions controller
    Introduce getUserSubscriptions in subscription.controller.js. The new handler verifies the authenticated user matches req.params.id, throws a 401 error if not the account owner, queries Subscription.find({ user: req.params.id }) and returns the subscriptions with a 200 response. Errors are forwarded to next().

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -12,3 +12,19 @@ export const createSubscription = async (req, res, next) => {
         next(e);
     }
 }
+
+export const getUserSubscriptions = async (req, res, next) => {
+    try {
+        if(req.user.id !== req.params.id) {
+            const error = new Error ('You are not the owner of this account');
+            error.status = 401;
+            throw error;
+        }
+
+        const subscriptions = await Subscription.find({ user: req.params.id});
+
+        res.status(200).json({ success: true, data: subscriptions});
+    } catch (e) {
+        next(e);
+    }
+}
