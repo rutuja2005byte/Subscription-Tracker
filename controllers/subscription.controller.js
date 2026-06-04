6bda563b0df6efee49961787c5226a4c32b8da1a
Rutuja <rutujadarade2005@gmail.com>
2026-06-04 16:35:33 +0530

    Make createSubscription async; fix auth import
    Add async to the createSubscription controller so awaited calls inside it work correctly and errors can be handled. Also update the auth middleware import in subscription.routes.js to include the .js extension to match ESM module resolution.

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,6 @@
 import Subscription from '../models/Subscription.model.js';
 
-export const createSubscription = (req, res, next) => {
+export const createSubscription = async (req, res, next) => {
     try {
         const subscription = await Subscription.create ({
             ...req.body,
--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import authorize from '../middlewares/auth.middleware';
+import authorize from '../middlewares/auth.middleware.js';
 import { createSubscription } from '../controllers/subscription.controller.js';
 
 const subscriptionRouter = Router();
